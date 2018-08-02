934fef98e3360f49ab3808a20dc327be6d0e1db8
Stefan Felder <st.felder@gmail.com>
2018-08-02 17:18:44 +0200

    test

--- WeatherAPI/WeatherDataManager.cs
+++ WeatherAPI/WeatherDataManager.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-//Wetter
+//Wetter2
             //https://openweathermap.org/weather-conditions
             //https://openweathermap.org/weather-data
 //Calling API http://openweathermap.org/api */
